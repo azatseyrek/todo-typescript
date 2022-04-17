d11f2123237a9d0ba086da936701769ed0f55e44
Azat Seyrek <azatseyrek@hotmail.com>
2022-04-18 01:29:42 +0300

    TodoList component start point

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import InputField from './components/InputField';
+import {Todo} from './components/types';
 import './App.css';
+import {getValue} from '@testing-library/user-event/dist/utils';
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('');
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    todo
+      ? setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}])
+      : setTodo('');
+  };
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <h2>typescript</h2>
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
